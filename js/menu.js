92094f4697afc61678cdc4f64a38fe6f0035db01
VladShapoval <vlllaad1992@gmail.com>
2022-05-23 21:07:46 +0300

    finish work 8

--- js/menu.js
+++ js/menu.js
@@ -3,4 +3,5 @@ const headerNav = document.querySelector(".header-nav")
 menuButton.addEventListener("click", () => {
     headerNav.classList.toggle("is-open")
     menuButton.classList.toggle("header-burger__button--is-open")
+    document.body.classList.toggle("modal-open")
 })
